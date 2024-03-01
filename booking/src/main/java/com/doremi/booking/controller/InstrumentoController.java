7892ae8170148206a71c643a594ac07f48a04974
capireque <carolina2001prq@gmail.com>
2024-02-29 21:15:44 -0500

    Ultimos cambios

--- booking/src/main/java/com/doremi/booking/controller/InstrumentoController.java
+++ booking/src/main/java/com/doremi/booking/controller/InstrumentoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @Slf4j
-@RestController
+@RestControllergit
 @RequestMapping("/instrumentos")
 public class InstrumentoController {
 
