1b5ebfaace351487656534001c0808e69be7aaf5
capireque <carolina2001prq@gmail.com>
2024-03-05 23:12:31 -0500

    WebCOnfigurationAdapter sin testear

---
+++ booking/src/main/java/com/doremi/booking/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.doremi.booking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(0, 0, 0, 0, 0);
+    }
+
+        
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       return http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/usuarios/**").hasAnyRole("USER", "ADMIN")
+            .requestMatchers("/instrumentos/agregar").hasRole("ADMIN")
+            .anyRequest().permitAll()
+        )
+        .formLogin(login -> login
+            .loginPage("/login")
+            .permitAll()
+        )
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+        )
+        .exceptionHandling(exceptionHandling -> exceptionHandling
+            .accessDeniedPage("/access-denied")
+        )
+        .build();
+     }
+               
+
+                
+                  
+    }
+
