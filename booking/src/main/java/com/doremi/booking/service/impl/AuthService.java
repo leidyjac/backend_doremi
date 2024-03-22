3a17cc7198f9c5c74e495fc5b86a64a6f5bc424d
capireque <carolina2001prq@gmail.com>
2024-03-22 07:54:02 -0500

    La rama que modifiqué, porque no estaba funcionando

---
+++ booking/src/main/java/com/doremi/booking/service/impl/AuthService.java
@@ -0,0 +1,54 @@
+package com.doremi.booking.service.impl;
+
+import java.util.List;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.doremi.booking.repository.UserRepository;
+import com.doremi.booking.Jwt.JwtService;
+import com.doremi.booking.dto.entrada.usuario.LoginRequest;
+import com.doremi.booking.dto.entrada.usuario.RegisterRequest;
+import com.doremi.booking.dto.salida.Usuario.AuthResponse;
+import com.doremi.booking.entity.Role;
+import com.doremi.booking.entity.User;
+import com.doremi.booking.exceptions.ResourceNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+            .token(token)
+            .build();
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+            .username(request.getUsername())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .nombre(request.getNombre())
+            .apellido(request.getApellido())
+            .role(Role.USER)
+            .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+            .token(jwtService.getToken(user))
+            .build();
+    }
+}
